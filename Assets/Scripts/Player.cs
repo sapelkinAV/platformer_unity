9ec4cd9d5f182c1c620ac40d85c0d29d01ee41fe
sapelkinAV <al.vi.sapelkin@gmail.com>
2021-05-09 17:00:06 +0300

    add animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
+    
 
     // Update is called once per frame
     void Update()
@@ -12,8 +13,9 @@ public class Player : MonoBehaviour
         var horizontal = Input.GetAxis("Horizontal");
         var rigidBody2D = GetComponent<Rigidbody2D>();
         rigidBody2D.velocity = new Vector2(horizontal * speed, rigidBody2D.velocity.y);
-        Debug.Log($"Velocity = {rigidBody2D.velocity}");
-        
-        
+
+        var animator = GetComponent<Animator>();
+        var walking = horizontal != 0;
+        animator.SetBool("Walk", walking);
     }
 }
